29bdb08d321e6febb5be05d6157b7c9d00ac17b0
Sierra Lopez <sierralopez1006@gmail.com>
2022-12-14 16:28:05 -0800

    store listings done

--- src/components/StoreListing.js
+++ src/components/StoreListing.js
@@ -11,7 +11,7 @@ export default function StoreListing() {
             <Row style={{ borderBottom: "1px solid #E8E9EB", display: "flex", justifyContent: "space-evenly"}} >
                 <Row>
                     <Col xs=".5" >
-                        <Image src="[IMG SRC]" roundedCircle />
+                        <Image src="https://www.instacart.com/image-server/87x87/filters:fill(FFF,true):format(webp)/www.instacart.com/assets/domains/warehouse/logo/38/bef2eb95-7375-4547-ab37-21502de090e0.png" roundedCircle  />
                     </Col>
                     <Col>
                         <h5 style={{ fontWeight: "bold" }}>Ralphs</h5>
@@ -21,20 +21,20 @@ export default function StoreListing() {
                 <Col sm="1"></Col>
                 <Row >
                     <Col xs=".5">
-                        <Image src="[IMG SRC]" roundedCircle />
+                        <Image src="https://www.instacart.com/image-server/87x87/filters:fill(FFF,true):format(webp)/www.instacart.com/assets/domains/warehouse/logo/279/59eb00b1-2e19-4912-bf2e-0c7a1f71f8b1.png" roundedCircle />
                     </Col>
                     <Col>
-                        <h5 style={{ fontWeight: "bold" }}>Vons</h5>
+                        <h5 style={{ fontWeight: "bold" }}>Sprouts Farmers Market</h5>
                         <p>Delivery * Pickup</p>
                     </Col>
                 </Row>
                 <Col sm="1"></Col>
                 <Row >
                     <Col xs=".5" >
-                        <Image src="[IMG SRC]" roundedCircle />
+                        <Image src="https://www.instacart.com/image-server/87x87/filters:fill(FFF,true):format(webp)/www.instacart.com/assets/domains/warehouse/logo/5/65f2304b-908e-4cd0-981d-0d4e4effa8de.png" roundedCircle />
                     </Col>
                     <Col md="6">
-                        <h5 style={{ fontWeight: "bold" }}>Trader Joe's</h5>
+                        <h5 style={{ fontWeight: "bold" }}>Costco</h5>
                         <p>Delivery</p>
                     </Col>
                 </Row>
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,7 +1,6 @@
 import { Navbar } from "react-bootstrap"
 import { Nav } from "react-bootstrap"
 import { Button } from "react-bootstrap"
-import '../App.css'
 
 export default function TopBar() {
     return (
